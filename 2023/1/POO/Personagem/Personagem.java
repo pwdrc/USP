1bad24fbf53c6f5e60cd312d6e63e834a23c1ac8
pwdrc <98365041+pwdrc@users.noreply.github.com>
2023-04-13 11:46:34 +0000

    finalizacao das classes

--- 2023/1/POO/Personagem/Personagem.java
+++ 2023/1/POO/Personagem/Personagem.java
@@ -1,7 +1,8 @@
 public class Personagem {
     private String nome;
     private String nomeVidaReal;
-    private ArrayList poderes;
+    private ArrayList<Superpoder> poderes;
+    private int vida;
 
     public String getNome() {
         return this.nome;
@@ -15,11 +16,31 @@ public class Personagem {
         return this.poderes.size();
     }
 
+    public int getVida() {
+        return this.vida;
+    }
+
     public void setNome(String pNome) {
         this.nome = pNome;
     }
 
     public void setNomeVidaReal(String pNomeVidaReal) {
         this.nomeVidaReal = pNomeVidaReal;
+    }
+
+    public void setVida(int pVida) {
+        this.vida = pVida;
+    }
+
+    public void atacar(int intAtaque, String nomePoder, Personagem atacado) {
+        
+        if(this.poderes.contains(nomePoder)) {
+            
+            int chanceDeAcerto = (int) (Math.random() * 100);
+            
+            if(chanceDeAcerto > 50 && atacado.vida > 0) {
+                atacado.vida -= intAtaque;
+            }
+        }
     }
 }
--- 2023/1/POO/Personagem/Superpoder.java
+++ 2023/1/POO/Personagem/Superpoder.java
@@ -1,3 +1,21 @@
 public class Superpoder {
     
+    private int categoria;
+    private String nome;
+
+    public int getCategoria() {
+        return this.categoria;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setCategoria(int pCategoria) {
+        this.categoria = pCategoria;
+    }
+
+    public void setNome(String pNome) {
+        this.nome = pNome;
+    }
 }
